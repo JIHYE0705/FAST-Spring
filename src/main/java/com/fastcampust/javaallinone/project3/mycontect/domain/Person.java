dde1ce35b3d76d75ef042dede9cde264c4da7dc7
{JIHYE0705} <{jihyem75@gmail.com}>
2021-06-06 19:57:46 +0900

    Lombok

--- src/main/java/com/fastcampust/javaallinone/project3/mycontect/domain/Person.java
+++ src/main/java/com/fastcampust/javaallinone/project3/mycontect/domain/Person.java
@@ -8,12 +8,10 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Data
 public class Person {
 
     @Id
@@ -26,6 +24,7 @@ public class Person {
 
     private int age;
     private String hobby;
+    @NonNull
     private String bloodType;
     private String address;
     private LocalDate birthday;
@@ -34,19 +33,5 @@ public class Person {
     @ToString.Exclude
     private String phoneNumber;
 
-    public boolean equals(Object object) {
-        if(object == null) {
-            return false;
-        }
-        Person person = (Person) object;
-        if(!person.getName().equals(this.getName())){
-            return false;
-        }
-        if(person.getAge() != this.getAge()){
-            return false;
-        }
 
-        return true;
-
-    }
 }
